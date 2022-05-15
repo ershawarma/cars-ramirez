1076c6aa44aedcdf426d46b5f2cddcd4301b9fe8
samuel <samuel.ssrc@gmail.com>
2022-05-14 21:12:23 -0500

    cambios may 14

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import ferrari from './ferrari.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ferrari} className="App-logo" alt="logo" />
+        <h3>
+          Cars Ramirez in progress
+        </h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ecomerce about cars sales
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
